bfa65962b45be308f710bf4bb6309ccd0ce20954
Lhumd <edaneshmand@tuebingen.mpg.de>
2020-02-28 11:21:31 +0100

    Add bolt controller

---
+++ python/py_blmc_controllers/bolt_centroidal_controller.py
@@ -0,0 +1,227 @@
+"""Centroidal
+
+"""
+
+
+import numpy as np
+import pinocchio as pin
+
+from py_blmc_controllers.qp_solver import quadprog_solve_qp
+
+arr = lambda a: np.array(a).reshape(-1)
+mat = lambda a: np.matrix(a).reshape((-1, 1))
+
+class BoltCentroidalController(object):
+    def __init__(self, robot, mass, mu, kp, kd, kpa, kda, eff_ids):
+        self._robot = robot
+        self._mass = mass # Total robot mass.
+        self._mu = mu # Friction coefficient
+        self._kp = kp
+        self._kd = kd
+        self._kpa = kpa
+        self._kda = kda
+        self._eff_ids = eff_ids
+
+    def compute_com_wrench(self, q, dq, des_com_pos, des_vel, des_ori, des_angvel):
+        """Compute the desired COM wrench (equation 1).
+
+        Args:o
+            des_com_pos: desired center of mass position at time t
+            des_vel: desired center of mass velocity at time t
+            des_ori: desired base orientation at time t (quaternions)
+            des_angvel: desired base angular velocity at time t
+        Returns:
+            Computed w_com
+        """
+        m = self._mass
+        robot = self._robot
+
+        com = arr(robot.com(q, dq)[0])
+        vcom = arr(robot.vcom(q, dq))
+        Ib = robot.mass(q)[3:6, 3:6]
+        quat_diff = self.quaternion_difference(arr(q[3:7]), arr(des_ori))
+
+        # print("______________________")
+        # print("quat_diff", quat_diff)
+        #print("arr(Ib * mat(np.multiply(self._kpa, quat_diff))):", arr(Ib * mat(np.multiply(self._kpa, quat_diff))))
+        # print("np.multiply(self._kpa, quat_diff)", np.multiply(self._kpa, quat_diff))
+        #print("mat(np.multiply(self._kpa, quat_diff))", mat(np.multiply(self._kpa, quat_diff)))
+        #print("Ib", Ib)
+        # print("kpa", self._kpa)
+        # print("np.multiply(self._kda, (des_angvel - arr(dq[3:6])))", np.multiply(self._kda, (des_angvel - arr(dq[3:6]))))
+
+        # from scipy.spatial.transform import Rotation as R
+        # print("current xyz", np.array(R.from_quat([np.array(q)[3:7, 0]]).as_euler('xyz', degrees=False))[0, :])
+        # print("desired xyz", np.array(R.from_quat([np.array(des_ori)[:]]).as_euler('xyz', degrees=False))[0, :])
+        # print("quat_diff", quat_diff)
+        from pyquaternion import Quaternion
+        import quaternion
+
+        # print(q[3])
+        # q1 = Quaternion([des_ori[3], des_ori[0], des_ori[1], des_ori[2]])
+        # q2 = Quaternion([q[6], q[3], q[4], q[5]])
+        # # p_q = Quaternion([des_angvel[6], des_angvel[3], des_angvel[4], des_angvel[5]])
+        # print("!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        # q3 = Quaternion.__mul__(q2.conjugate, q1)
+        # print("dif1", Quaternion.__sub__(q1, q2))
+        # print("dif2", Quaternion.__mul__(q2.conjugate, q1)) #Lhum nemidonam chera barabar nistan dif1 != dif2
+        # print("log1", Quaternion.log(q3))
+        # print("log2", Quaternion.log(Quaternion.__sub__(q1, q2)))
+        # print("quat_diff", quat_diff)# quat_diff = log1 * 2
+        # # print("sub prev - now", Quaternion.exp(Quaternion.__mul__(p_q.conjugate, q3)) * 1000)
+        # print("dq", arr(dq[3:6]))
+        #
+        # print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+        #
+        # print(des_angvel)
+        # print(np.matrix([[dq[3].item()], [dq[4].item()], [dq[5].item()]]))
+        # print(dq[3:6])
+        # print(arr(dq[3:6]))
+        # print([dq[3].item(), dq[4].item(), dq[5].item()])
+        w_com = np.hstack([
+            np.multiply(self._kp, (des_com_pos - com)) + np.multiply(self._kd, (des_vel - vcom)),
+            np.multiply(self._kpa, quat_diff) + np.multiply(self._kda, (des_angvel -
+            arr(np.matrix([[dq[3].item()], [dq[4].item()], [dq[5].item()]]))))#np.matrix([[dq[3].item()], [dq[4].item()], [dq[5].item()]])))
+        ])
+        # print(np.multiply(self._kda, (des_angvel -
+        #     arr(np.matrix([[dq[3].item()], [dq[4].item()], [dq[5].item()]])))))
+        # print(np.multiply(self._kpa, quat_diff))
+        return w_com
+
+    def compute_force_qp(self, q, dq, cnt_array, w_com):
+        """Computes the forces needed to generated a desired centroidal wrench.
+
+        Args:
+            q: Generalized robot position configuration.
+            dq: Generalized robot velocity configuration.
+            cnt_array: Array with {0, 1} of #endeffector size indicating if
+                an endeffector is in contact with the ground or not. Forces are
+                only computed for active endeffectors.
+            w_com: Desired centroidal wrench to achieve given forces.
+        Returns:
+            Computed forces as a plain array of size 3 * num_endeffectors.
+        """
+        robot = self._robot
+        com = robot.com(q, dq)[0]
+        r = [robot.data.oMf[i].translation - com for i in self._eff_ids]
+        # print(r)
+        # print(r[0])
+        # r[0][2] = 0
+        # r[1][2] = 0
+
+        # Use the contact activation from the plan to determine which of the forces
+        # should be active.
+        N = (int)(np.sum(cnt_array))
+
+        # Setup the QP problem.
+        Q = 2. * np.eye(3 * N + 6)
+        Q[-6:,-6:] = 1 * np.eye(6)
+        Q[-4:-1,-4:-1] = 1e6 * np.eye(3)
+        p = np.zeros(3 * N + 6)
+        A = np.zeros((6, 3 * N + 6))
+        b = w_com
+
+        condition_number = 5
+        G = np.zeros((condition_number * N, 3 * N + 6))
+        h = np.zeros((condition_number * N))
+
+        j = 0
+        for i in range(2):
+            if cnt_array[i] == 0:
+                continue
+
+            A[:3, 3 * j:3 * (j + 1)] = np.eye(3)
+            A[3:, 3 * j:3 * (j + 1)] = pin.utils.skew(r[i])
+
+            G[condition_number*j + 0, 3 * j + 0] = 1    # mu Fz - Fx >= 0
+            G[condition_number*j + 0, 3 * j + 2] = -self._mu
+            G[condition_number*j + 1, 3 * j + 0] = -1     # mu Fz + Fx >= 0
+            G[condition_number*j + 1, 3 * j + 2] = -self._mu
+            G[condition_number*j + 2, 3 * j + 1] = 1    # mu Fz - Fy >= 0
+            G[condition_number*j + 2, 3 * j + 2] = -self._mu
+            G[condition_number*j + 3, 3 * j + 1] = -1     # mu Fz + Fy >= 0
+            G[condition_number*j + 3, 3 * j + 2] = -self._mu
+            G[condition_number*j + 4, 3 * j + 2] = -1     # Fz >= 0
+
+            j += 1
+        A[:, -6:] = np.eye(6)
+        # A[2,N*3 +2] = 0
+        #A[3,N*3 +3] = 0
+        #A[-4,-4] = 0
+        #A[2,2] = 1e6
+
+        solx = quadprog_solve_qp(Q, p, G, h, A, b)
+        F = np.zeros(6)
+        j = 0
+
+        for i in range(2):
+            if cnt_array[i] == 0:
+                continue
+            F[3*i: 3*(i + 1)] = solx[3*j: 3*(j + 1)]
+            j += 1
+
+        return F
+
+
+    #### quaternion stuff
+    def skew(self, v):
+        '''converts vector v to skew symmetric matrix'''
+        assert v.shape[0] == 3, 'vector dimension is not 3 in skew method'
+        return np.array([[0., -v[2], v[1]],
+                         [v[2], 0., -v[0]],
+                         [-v[1], v[0], 0.]])
+    def quaternion_to_rotation(self, q):
+        ''' converts quaternion to rotation matrix '''
+        return (q[3]**2 - q[:3].dot(q[:3]))*np.eye(3) \
+            + 2. * np.outer(q[:3], q[:3]) + 2.*q[3]*self.skew(q[:3])
+
+    def exp_quaternion(self, w):
+        ''' converts angular velocity to quaternion '''
+        qexp = np.zeros(4)
+        th = np.linalg.norm(w)
+        if th**2 <= 1.e-6:
+            ''' small norm causes closed form to diverge,
+            use taylor expansion to approximate '''
+            qexp[:3] = (1-(th**2)/6)*w
+            qexp[3] = 1-(th**2)/2
+        else:
+            u = w/th
+            qexp[:3] = np.sin(th)*u
+            qexp[3] = np.cos(th)
+        return qexp
+
+    def log_quaternion(self, q):
+        """ lives on the tangent space of SO(3) """
+        v = q[:3]
+        w = q[3]
+        vnorm = np.linalg.norm(v)
+        if vnorm <= 1.e-6:
+            return 2 * v/w * (1 - vnorm**2/(3*w**2))
+        else:
+            return 2*np.arctan2(vnorm, w) * v / vnorm
+        #https://math.stackexchange.com/questions/2552/the-logarithm-of-quaternion/2554#2554
+        #ln𝐪=(ln|𝐪|,(1‖𝑣‖arccos𝑠|𝑞|)𝑣)
+
+    def quaternion_product(self, q1, q2):
+        """ computes quaternion product of q1 x q2 """
+        p = np.zeros(4)
+        p[:3] = np.cross(q1[:3], q2[:3]) + q2[3]*q1[:3] + q1[3]*q2[:3]
+        p[3] = q1[3]*q2[3] - q1[:3].dot(q2[:3])
+        return p
+
+    def integrate_quaternion(self, q, w):
+        """ updates quaternion with tangent vector w """
+        dq = self.exp_quaternion(.5*w)
+        return self.quaternion_product(dq, q)
+
+    def quaternion_difference(self, q1, q2):
+        """computes the tangent vector from q1 to q2 at Identity
+        returns vecotr w
+        s.t. q2 = exp(.5 * w)*q1
+        """
+        # first compute dq s.t.  q2 = q1*dq
+        q1conjugate = np.array([-q1[0], -q1[1], -q1[2], q1[3]])
+        # order of multiplication is very essential here
+        dq = self.quaternion_product(q2, q1conjugate)
+        # increment is log of dq
+        return self.log_quaternion(dq)
