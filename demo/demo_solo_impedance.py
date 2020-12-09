c5027beeb32ff86a36b5d7303ead7a5b7d71da71
Avadesh Meduri <avadesh02@gmail.com>
2020-12-09 13:23:13 -0500

    shifting from gitlab to github

--- demo/demo_solo_impedance.py
+++ demo/demo_solo_impedance.py
@@ -13,10 +13,9 @@ import os
 import rospkg
 import pybullet as p
 import pinocchio as se3
-from pinocchio.utils import se3ToXYZQUAT
 
-from robot_properties_solo.config import Solo12Config
-from robot_properties_solo.quadruped12wrapper import Quadruped12Robot
+from bullet_utils.env import BulletEnvWithGround
+from robot_properties_solo.solo12wrapper import Solo12Robot, Solo12Config
 
 from py_blmc_controllers.solo_impedance_controller import SoloImpedanceController
 
@@ -24,16 +23,19 @@ from pinocchio.utils import zero
 from matplotlib import pyplot as plt
 
 
-# Create a robot instance. This initializes the simulator as well.
-robot = Quadruped12Robot()
-tau = np.zeros(12)
+# Create a Pybullet simulation environment
+env = BulletEnvWithGround()
 
-# Reset the robot to some initial state.
+# Create a robot instance. This initializes the simulator as well.
+robot = env.add_robot(Solo12Robot)
+tau = np.zeros(robot.nb_dof)
+
+# # Reset the robot to some initial state.
 q0 = np.matrix(Solo12Config.initial_configuration).T
 dq0 = np.matrix(Solo12Config.initial_velocity).T
 robot.reset_state(q0, dq0)
 
-###################### impedance controller demo #################################
+# ###################### impedance controller demo #################################
 
 x_des = 4*[0.0, 0.0, -0.25]
 xd_des = 4*[0,0,0]
@@ -41,17 +43,16 @@ kp = 4 * [200,200,200]
 kd = 4 * [10.0,10.0,10.0]
 f = np.zeros(18)
 f = 4*[0.0, 0.0, (2.2*9.8)/4]
-##################################################################################
+# ##################################################################################
 
-solo_leg_ctrl = SoloImpedanceController(robot)
+# solo_leg_ctrl = SoloImpedanceController(robot)
 
-# Run the simulator for 100 steps
+# # Run the simulator for 100 steps
 for i in range(4000):
     # TODO: Implement a controller here.
     # Step the simulator.
-    p.stepSimulation()
-    time.sleep(0.001) # You can sleep here if you want to slow down the replay
+    env.step(sleep=True) # You can sleep here if you want to slow down the replay
     # Read the final state and forces after the stepping.
     q, dq = robot.get_state()
-    tau = solo_leg_ctrl.return_joint_torques(q,dq,kp,kd,x_des,xd_des,f)
+    # tau = solo_leg_ctrl.return_joint_torques(q,dq,kp,kd,x_des,xd_des,f)
     robot.send_joint_command(tau)
