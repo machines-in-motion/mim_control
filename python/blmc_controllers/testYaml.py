58db6988a35036c1691c230a1b26b342e5db95c2
Neotriple <neotriple@gmail.com>
2020-12-10 00:24:05 +0000

    Removed extraneous files and fixed spelling of demo_impedance_controller

--- python/blmc_controllers/testYaml.py
+++
@@ -1,8 +0,0 @@
-import numpy
-import yaml
-
-config_file = 'yaml_example.yaml'
-with open(config_file) as config:
-	data_in = yaml.safe_load(config)
-for ctrls in data_in["impedance_controllers"]:
-	print data_in["impedance_controllers"][ctrls]["is_eeff"]
