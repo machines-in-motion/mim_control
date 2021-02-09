cc38a27eff878b2321f908532d3ae7acd26395cd
Maximilien Naveau <maximilien.naveau@gmail.com>
2021-02-09 18:03:28 +0100

    add the files for the entity, fixed the impedance_controller header.

---
+++ include/mim_control/dynamic_graph/impedance_controller.hpp
@@ -0,0 +1,135 @@
+/**
+ * @file
+ * @license BSD 3-clause
+ * @copyright Copyright (c) 2020, New York University and Max Planck
+ * Gesellschaft
+ *
+ * @brief This is the implementation for impedance controller between any two
+ * frames of the robot.
+ *
+ */
+
+#pragma once
+
+#include "dynamic-graph/all-signals.h"
+#include "dynamic-graph/entity.h"
+#include "mim_control/impedance_controller.hpp"
+
+namespace mim_control
+{
+namespace dynamic_graph
+{
+/**
+ * @brief Entity around the ImpedanceController class of this package.
+ */
+class ImpedanceController : public dynamicgraph::Entity
+{
+    DYNAMIC_GRAPH_ENTITY_DECL();
+
+public:
+    /**
+     * @brief Construct a new ImpedanceController object using its Dynamic Graph
+     * name.
+     *
+     * @param name
+     */
+    ImpedanceController(const std::string& name);
+
+    /**
+     * @brief Initialize the internal data. None real-time safe method.
+     *
+     * @param pinocchio_model rigid body model of the robot
+     * @param root_frame_name root frame name where the spring starts(Ex. Hip)
+     * @param end_frame_name frame name where the spring ends(Ex. end effector)
+     */
+    void initialize(const pinocchio::Model& pinocchio_model,
+                    const std::string& root_frame_name,
+                    const std::string& end_frame_name);
+
+    /*
+     * Input Signals.
+     */
+
+    /** @brief Robot generalized coordinates (q). */
+    dynamicgraph::SignalPtr<dynamicgraph::Vector, int> robot_configuration_sin_;
+
+    /** @brief Robot generalized velocity (dq). */
+    dynamicgraph::SignalPtr<dynamicgraph::Vector, int> robot_velocity_sin_;
+
+    /** @brief Proportional gain from the cartesian PD. */
+    dynamicgraph::SignalPtr<dynamicgraph::Vector, int> gain_proportional_sin_;
+
+    /** @brief Proportional gain from the cartesian PD. */
+    dynamicgraph::SignalPtr<dynamicgraph::Vector, int> gain_derivative_sin_;
+
+    /** @brief Gain multiplying the feedforward force. */
+    dynamicgraph::SignalPtr<double, int> gain_feed_forward_force_sin_;
+
+    /** @brief */
+    dynamicgraph::SignalPtr<dynamicgraph::Vector, int>
+        desired_end_frame_placement_sin_;
+
+    /** @brief */
+    dynamicgraph::SignalPtr<dynamicgraph::Vector, int>
+        desired_end_frame_velocity_sin_;
+
+    /** @brief */
+    dynamicgraph::SignalPtr<dynamicgraph::Vector, int> feed_forward_force_sin_;
+
+    /*
+     * Output Signals.
+     */
+
+    /** @brief Internal transition signal. */
+    dynamicgraph::SignalTimeDependent<bool, int> one_iteration_sout_;
+
+    /** @brief Output joint torques. */
+    dynamicgraph::SignalTimeDependent<dynamicgraph::Vector, int> torque_sout_;
+
+    /** @brief Output generalized torques. */
+    dynamicgraph::SignalTimeDependent<dynamicgraph::Vector, int>
+        joint_torque_sout_;
+
+    /** @brief Impedance forces computed. */
+    dynamicgraph::SignalTimeDependent<dynamicgraph::Vector, int>
+        impedance_force_;
+
+protected:
+    /**
+     * @brief Callback function of the torque_sout_ signal.
+     *
+     * @param torque
+     * @param time
+     * @return dynamicgraph::Vector&
+     */
+    dynamicgraph::Vector& torque_callback(dynamicgraph::Vector& torque,
+                                          int time);
+
+    /**
+     * @brief Callback function of the joint_torque_sout_ signal.
+     *
+     * @param joint_torque
+     * @param time
+     * @return dynamicgraph::Vector&
+     */
+    dynamicgraph::Vector& joint_torque_callback(
+        dynamicgraph::Vector& joint_torque, int time);
+
+    /**
+     * @brief Callback function of the impedance_force_sout_ signal.
+     *
+     * @param impedance_force
+     * @param time
+     * @return dynamicgraph::Vector&
+     */
+    dynamicgraph::Vector& impedance_force_callback(
+        dynamicgraph::Vector& impedance_force, int time);
+
+    /**
+     * @brief Internally calls the ImpedanceController class.
+     */
+    bool& one_iteration(bool& dummy, int& time);
+};
+
+}  // namespace dynamic_graph
+}  // namespace mim_control
---
+++ src/dynamic_graph/impedance_controller.cpp
@@ -0,0 +1,151 @@
+/**
+ * @file
+ * @license BSD 3-clause
+ * @copyright Copyright (c) 2020, New York University and Max Planck
+ * Gesellschaft
+ *
+ * @brief Implementation of the ImpedanceController class.
+ */
+
+#include "mim_control/impedance_controller.hpp"
+#include "pinocchio/algorithm/frames.hpp"
+
+namespace mim_control
+{
+ImpedanceController::ImpedanceController()
+{
+}
+
+void ImpedanceController::initialize(const pinocchio::Model& pinocchio_model,
+                                     const std::string& root_frame_name,
+                                     const std::string& end_frame_name)
+{
+    // Copy the arguments internally.
+    pinocchio_model_ = pinocchio_model;
+    root_frame_name_ = root_frame_name;
+    end_frame_name_ = end_frame_name;
+
+    // Create the cache of the rigid body dynamics algorithms
+    pinocchio_data_ = pinocchio::Data(pinocchio_model_);
+
+    // Fetch the index of the frame in the robot model.
+    root_frame_index_ = pinocchio_model_.getFrameId(root_frame_name_);
+    end_frame_index_ = pinocchio_model_.getFrameId(end_frame_name_);
+
+    // initialize the size of the vectors.
+    root_jacobian_.resize(6, pinocchio_model_.nv);
+    root_jacobian_.fill(0.);
+    end_jacobian_.resize(6, pinocchio_model_.nv);
+    end_jacobian_.fill(0.);
+    impedance_jacobian_.resize(6, pinocchio_model_.nv);
+    impedance_jacobian_.fill(0.);
+
+    // Defines if the model has a freeflyer.
+    pinocchio_model_has_free_flyer_ =
+        pinocchio_model_.joints[1].shortname() == "JointModelFreeFlyer";
+
+    // output
+    torques_.resize(pinocchio_model_.nv, 1);
+    torques_.fill(0.);
+    if(pinocchio_model_has_free_flyer_)
+        joint_torques_.resize(pinocchio_model_.nv, 1);
+    else{
+        joint_torques_.resize(pinocchio_model_.nv - 6, 1);
+    }
+}
+
+void ImpedanceController::run(
+    Eigen::Ref<const Eigen::VectorXd> robot_configuration,
+    Eigen::Ref<const Eigen::VectorXd> robot_velocity,
+    Eigen::Ref<const Array6d> gain_proportional,
+    Eigen::Ref<const Array6d> gain_derivative,
+    const double& gain_feed_forward_force,
+    const pinocchio::SE3& desired_end_frame_placement,
+    const pinocchio::Motion& desired_end_frame_velocity,
+    const pinocchio::Force& feed_forward_force)
+{
+    assert(robot_configuration.size() == pinocchio_model_.nq &&
+           "robot_configuration is not of the good size.");
+    assert(robot_velocity.size() == pinocchio_model_.nv &&
+           "robot_velocity is not of the good size.");
+
+    // Get the current frame placements and velocity.
+    pinocchio::forwardKinematics(
+        pinocchio_model_, pinocchio_data_, robot_configuration, robot_velocity);
+    pinocchio::updateFramePlacement(
+        pinocchio_model_, pinocchio_data_, root_frame_index_);
+    pinocchio::updateFramePlacement(
+        pinocchio_model_, pinocchio_data_, end_frame_index_);
+
+    root_placement_ = pinocchio_data_.oMf[root_frame_index_];
+    end_placement_ = pinocchio_data_.oMf[end_frame_index_];
+    root_velocity_ =
+        pinocchio::getFrameVelocity(pinocchio_model_,
+                                    pinocchio_data_,
+                                    root_frame_index_,
+                                    pinocchio::LOCAL_WORLD_ALIGNED);
+    end_velocity_ = pinocchio::getFrameVelocity(pinocchio_model_,
+                                                pinocchio_data_,
+                                                end_frame_index_,
+                                                pinocchio::LOCAL_WORLD_ALIGNED);
+
+    // Compute the force to be applied to the environment.
+    const pinocchio::SE3 diff = desired_end_frame_placement.actInv(
+        root_placement_.actInv(end_placement_));
+    impedance_force_.head(3) =
+        -gain_proportional.head(3) * diff.translation().array();
+    impedance_force_.tail(3) =
+        -gain_proportional.tail(3) * pinocchio::log3(diff.rotation()).array();
+
+    impedance_force_ += (gain_derivative * (desired_end_frame_velocity -
+                                            (end_velocity_ - root_velocity_))
+                                               .toVector()
+                                               .array())
+                            .matrix();
+
+    impedance_force_ -=
+        (gain_feed_forward_force * feed_forward_force.toVector().array())
+            .matrix();
+
+    // Compute the jacobians
+    pinocchio::computeJointJacobians(
+        pinocchio_model_, pinocchio_data_, robot_configuration);
+    pinocchio::getFrameJacobian(pinocchio_model_,
+                                pinocchio_data_,
+                                end_frame_index_,
+                                pinocchio::LOCAL_WORLD_ALIGNED,
+                                end_jacobian_);
+    pinocchio::getFrameJacobian(pinocchio_model_,
+                                pinocchio_data_,
+                                root_frame_index_,
+                                pinocchio::LOCAL_WORLD_ALIGNED,
+                                root_jacobian_);
+    impedance_jacobian_ = end_jacobian_ - root_jacobian_;
+
+    // compute the output torques
+    torques_ = (impedance_jacobian_.transpose() * impedance_force_);
+    
+    if(pinocchio_model_has_free_flyer_)
+        joint_torques_ = torques_.tail(pinocchio_model_.nv - 6);
+    else{
+        joint_torques_ = torques_;
+    }
+    return;
+}
+
+const Eigen::VectorXd& ImpedanceController::get_torques()
+{
+    return torques_;
+}
+
+const Eigen::VectorXd& ImpedanceController::get_joint_torques()
+{
+    return joint_torques_;
+}
+
+const ImpedanceController::Vector6d& ImpedanceController::get_impedance_force()
+{
+    return impedance_force_;
+}
+
+}  // namespace mim_control
