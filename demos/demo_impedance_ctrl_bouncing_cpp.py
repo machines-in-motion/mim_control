e5e30c0c280c15e25cbaf55908edca380b0042ff
Maximilien Naveau <maximilien.naveau@gmail.com>
2021-03-16 16:09:11 +0100

    add a falling and rebound demo

--- demos/demo_impedance_ctrl_bouncing_cpp.py
+++ demos/demo_impedance_ctrl_bouncing_cpp.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
     # Create a Pybullet simulation environment
     env = BulletEnvWithGround()
-    robot = env.add_robot(Solo12Robot, useFixedBase=True)
+    robot = env.add_robot(Solo12Robot, useFixedBase=False)
     pybullet.resetDebugVisualizerCamera(1.3, 100, -35, (0.0, 0.0, 0.0))
     RobotConfig = Solo12Config
     pin_robot = robot.pin_robot
@@ -51,70 +51,62 @@ if __name__ == "__main__":
         pinocchio.SE3(np.identity(3), np.array([-0.195, -0.147, -0.2])),
     ]
     xd_des = pinocchio.Motion.Zero()
-    kp = np.array([70, 70, 70, 0, 0, 0])
-    kd = np.array([20.0, 20.0, 20.0, 0, 0, 0])
+    kp = np.array([200, 200, 200, 0, 0, 0])
+    kd = np.array([5.0, 5.0, 5.0, 0, 0, 0])
     f = pinocchio.Force.Zero()
 
     end_eff_ids = [
         pin_robot.model.getFrameId(endeff_name) for endeff_name in endeff_names
     ]
 
+    # Reset the robot to some initial state.
+    q0 = np.array(RobotConfig.initial_configuration)
+    dq0 = np.array(RobotConfig.initial_velocity)
+    q0[0] += 0.0
+    q0[1] += 0.0
+    q0[2] += 0.5
+    q0[3:7] = np.array([0.0, 0.0, 0.0, 1.0])
+    robot.reset_state(q0, dq0)
+
     time = 0
-    N = 10
-    for n in range(N + 1):
-        # Reset the robot to some initial state.
-        q0 = np.array(RobotConfig.initial_configuration)
-        dq0 = np.array(RobotConfig.initial_velocity)
+    for i in range(2000):
+        q, dq = robot.get_state()
+        tau.fill(0)
+        tau_imp.fill(0)
+        tau_pd.fill(0)
 
-        q0[0] += 0.0
-        q0[1] += 0.2
-        q0[2] += 0.2
-        q0[3:7] = pinocchio.Quaternion(
-            pinocchio.rpy.rpyToMatrix(
-                np.array([float(n) / float(N) * 2 * np.pi, 0, 0])
+        tau_pd[:] = -2.0 * q[7:] - 0.1 * dq[6:]
+
+        for i, ctrl in enumerate(ctrls):
+            ctrl.run(
+                q,
+                dq,
+                kp,
+                kd,
+                0.0,
+                x_des[i],
+                xd_des,
+                f,
             )
-        ).coeffs()
+            tau_imp += ctrl.get_joint_torques()
 
-        robot.reset_state(q0, dq0)
+        for i in range(robot.nb_dof):
+            if tau_imp[i] != 0.0:
+                tau[i] = tau_imp[i]
+            else:
+                tau[i] = tau_pd[i]
 
-        for i in range(300):
-            q, dq = robot.get_state()
-            tau.fill(0)
-            tau_imp.fill(0)
-            tau_pd.fill(0)
-
-            tau_pd[:] = -2.0 * q[7:] - 0.1 * dq[6:]
-
-            for i, ctrl in enumerate(ctrls):
-                ctrl.run(
-                    q,
-                    dq,
-                    kp,
-                    kd,
-                    0.0,
-                    x_des[i],
-                    xd_des,
-                    f,
-                )
-                tau_imp += ctrl.get_joint_torques()
-
-            for i in range(robot.nb_dof):
-                if tau_imp[i] != 0.0:
-                    tau[i] = tau_imp[i]
-                else:
-                    tau[i] = tau_pd[i]
-
-            time += 1
-            robot.send_joint_command(tau)
-            data.add_variable(time, "time", "millisec")
-            data.add_matrix(tau, "tau", "Nm")
-            data.add_matrix(tau_pd, "tau_pd", "Nm")
-            data.add_matrix(tau_imp, "tau_imp", "Nm")
-            data.add_se3(x_des[0], "fl_pos")
-            data.add_se3(x_des[1], "fr_pos")
-            data.add_se3(x_des[2], "hl_pos")
-            data.add_se3(x_des[3], "hr_pos")
-            env.step()
+        time += 1
+        robot.send_joint_command(tau)
+        data.add_variable(time, "time", "millisec")
+        data.add_matrix(tau, "tau", "Nm")
+        data.add_matrix(tau_pd, "tau_pd", "Nm")
+        data.add_matrix(tau_imp, "tau_imp", "Nm")
+        data.add_se3(x_des[0], "fl_pos")
+        data.add_se3(x_des[1], "fr_pos")
+        data.add_se3(x_des[2], "hl_pos")
+        data.add_se3(x_des[3], "hr_pos")
+        env.step()
 
     empty_view = {"Empty View": {0: []}}
     my_session = Session()
